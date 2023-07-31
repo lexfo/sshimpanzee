1839d856541d54895784bc47603d2aff05652059
Titouan Lazard <tlazard@lexfo.fr>
2023-07-31 15:24:30 +0200

    Modifyng error message

--- tuns/combined/combiner_template.c
+++ tuns/combined/combiner_template.c
@@ -4,14 +4,13 @@
 int tun(){
   char *selected_tun;
   selected_tun  = getenv("MODE");
-  puts("EXPLOITING TUN COMBINER");
   if (selected_tun != 0)
     {
       printf("Trying tun name: %s\n", selected_tun);
     }
   else
     {
-      puts("You need to provide a tun");
+      puts("You need to provide a tun, use TUN env var");
       exit(-1);
     }
   <CONTENT>
