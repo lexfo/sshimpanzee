efccd75a5a541a041c171019c62ac90f4f3d2cae
Titouan Lazard <tlazard@lexfo.fr>
2023-08-05 16:38:36 +0200

    Adding options to ignore invalide TLS certificate

--- utils/client/bin/proxy_cli.py
+++ utils/client/bin/proxy_cli.py
@@ -26,26 +26,26 @@ parser.add_argument("--push", type=list, help='push a file on the remote filesys
 parser.add_argument("--user-agent", type=str, help='Custom user agent', default='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36')
 
 parser.add_argument("--proxy", '-x', type=str, help='Proxy')
+parser.add_argument("--no-tls-verify", "-k", default=True, action='store_const', const=False)
 
 
-
-def drop(pth, what, where):
+def drop(pth, what, where, verify=True):
     with open(what, 'rb') as reader:
         r = reader.read(1024)
         c = r
         while len(r)==1024:
             r = reader.read()
         b64 = base64.b64encode(c)
-        x = requests.post(pth, {"TODO": "DROP", "KEY": key, "WHAT":b64, "WHERE" : where}, headers=headers, proxies=proxies, timeout=7)
+        x = requests.post(pth, {"TODO": "DROP", "KEY": key, "WHAT":b64, "WHERE" : where}, headers=headers, proxies=proxies, timeout=7, verify=verify)
     
         
-def run(pth, where):
-    x = requests.post(pth, {"TODO": "START", "KEY": key, "WHERE" : where}, headers=headers, proxies=proxies, timeout=7)
+def run(pth, where, verify=True):
+    x = requests.post(pth, {"TODO": "START", "KEY": key, "WHERE" : where}, headers=headers, proxies=proxies, timeout=7,  verify=verify)
     print(x.content)
     
 
-def pull_data(pth):
-    x = requests.post(pth, {"TODO": "READ", "KEY":key}, headers=headers, proxies=proxies, timeout=7)
+def pull_data(pth, verify=True):
+    x = requests.post(pth, {"TODO": "READ", "KEY":key}, headers=headers, proxies=proxies, timeout=7, verify=verify)
     if (not x or x.status_code == 201):
         print("sshd not running",  file=sys.stderr)
         sys.exit(-1)    
@@ -58,9 +58,9 @@ def pull_data(pth):
     return
 
 
-def write_data(pth,d):
+def write_data(pth,d, verify=True):
     b = base64.b64encode(d)
-    x = requests.post(pth, {"TODO":"WRITE" , "DATA":b, "KEY": key}, headers=headers, proxies=proxies)
+    x = requests.post(pth, {"TODO":"WRITE" , "DATA":b, "KEY": key}, headers=headers, proxies=proxies, verify=verify)
     #print(x.content, file=sys.stderr)
     return x.status_code
 
@@ -86,13 +86,13 @@ if __name__ == "__main__":
                 ins, _, _  = select.select(i, [], [], 0)
             if len(ins) != 0:
                 x = sys.stdin.buffer.read()
-                write_data(args.url,x)
-            pull_data(args.url)
+                write_data(args.url,x,  verify=args.no_tls_verify)
+            pull_data(args.url,  verify=args.no_tls_verify)
     if args.push:
-        drop(args.url, args.push[0], args.push[1])
+        drop(args.url, args.push[0], args.push[1],  verify=args.no_tls_verify)
 
     elif args.run:
-        run(args.url, args.run)
+        run(args.url, args.run,  verify=args.no_tls_verify)
 
 
         
